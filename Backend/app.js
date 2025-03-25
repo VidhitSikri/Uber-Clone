001e42ede9a69aa7ea5b1fd587f7b1c189db807b
VidhitSikri <sikrividhit@gmail.com>
2025-03-25 16:34:24 +0530

    added more backend code

--- Backend/app.js
+++ Backend/app.js
@@ -4,10 +4,20 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const userRoutes = require("./routes/user.routes");
+
 
 const cors = require("cors");
 app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const connectToDb = require("./db/db");
+connectToDb();
+
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {   
     res.send("Hello World!");
