1e96873a0911c924d1337e249b2e8db10755224c
VidhitSikri <sikrividhit@gmail.com>
2025-03-26 02:48:16 +0530

    added complete authentication for captain as well as user

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,9 +1,9 @@
-const captainModel = require("../models/captain.model");
-const captainService = require("../services/captain.service");
+const captainModel = require('../models/captain.model');
+const captainService = require('../services/captain.service');
+const blacklistTokenModel = require('../models/blacklistTokens.model'); // Corrected import
 const { validationResult } = require('express-validator');
 
 module.exports.registerCaptain = async (req, res, next) => {
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -16,7 +16,6 @@ module.exports.registerCaptain = async (req, res, next) => {
     if (isCaptainAlreadyExist) {
         return res.status(400).json({ message: 'Captain already exist' });
     }
-
 
     const hashedPassword = await captainModel.hashPassword(password);
 
@@ -34,5 +33,45 @@ module.exports.registerCaptain = async (req, res, next) => {
     const token = captain.generateAuthToken();
 
     res.status(201).json({ token, captain });
+}
 
+module.exports.loginCaptain = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const captain = await captainModel.findOne({ email }).select('+password');
+
+    if (!captain) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const isMatch = await captain.comparePassword(password);
+
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const token = captain.generateAuthToken();
+
+    res.cookie('token', token);
+
+    res.status(200).json({ token, captain });
+}
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+    res.status(200).json({ captain: req.captain });
+}
+
+module.exports.logoutCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+    await blacklistTokenModel.create({ token });
+
+    res.clearCookie('token');
+
+    res.status(200).json({ message: 'Logout successfully' });
 }
--- Backend/services/captain.service.js
+++ Backend/services/captain.service.js
@@ -1,12 +1,11 @@
-const captainModel = require("../models/captain.model");
+const captainModel = require('../models/captain.model');
 
 
-
-module.exports.createCaptain = async ({ 
+module.exports.createCaptain = async ({
     firstname, lastname, email, password, color, plate, capacity, vehicleType
- }) => {
-    if(!firstname || !email || !password || !color || !plate || !capacity || !vehicleType){
-        throw new Error("Please enter all the required fields");
+}) => {
+    if (!firstname || !email || !password || !color || !plate || !capacity || !vehicleType) {
+        throw new Error('All fields are required');
     }
     const captain = captainModel.create({
         fullname: {
@@ -21,6 +20,7 @@ module.exports.createCaptain = async ({
             capacity,
             vehicleType
         }
-    });
+    })
+
     return captain;
 }
