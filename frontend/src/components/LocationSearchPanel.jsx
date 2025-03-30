42ee3e7f0fb642120409bafce2b7dda0f1b0cd21
VidhitSikri <sikrividhit@gmail.com>
2025-03-31 02:44:58 +0530

    added chose vehicle and select pickup and destination components o the home page

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -2,8 +2,36 @@ import React from 'react'
 
 const LocationSearchPanel = () => {
   return (
-    <div>
-      location search panel
+    <div className='flex flex-col gap-5'>
+      {/* this is just the sample data */}
+      <div className='flex items-center justify-start gap-5'>
+        <h2><i class="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B, Near Kapoor's cafe , Sheriyan's Coding School, Bhopal</h4>
+      </div>
+      <div className='flex items-center justify-start gap-5'>
+        <h2><i class="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B, Near Kapoor's cafe , Sheriyan's Coding School, Bhopal</h4>
+      </div>
+      <div className='flex items-center justify-start gap-5'>
+        <h2><i class="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B, Near Kapoor's cafe , Sheriyan's Coding School, Bhopal</h4>
+      </div>
+      <div className='flex items-center justify-start gap-5'>
+        <h2><i class="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B, Near Kapoor's cafe , Sheriyan's Coding School, Bhopal</h4>
+      </div>
+      <div className='flex items-center justify-start gap-5'>
+        <h2><i class="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B, Near Kapoor's cafe , Sheriyan's Coding School, Bhopal</h4>
+      </div>
+      <div className='flex items-center justify-start gap-5'>
+        <h2><i class="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B, Near Kapoor's cafe , Sheriyan's Coding School, Bhopal</h4>
+      </div>
+      <div className='flex items-center justify-start gap-5'>
+        <h2><i class="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B, Near Kapoor's cafe , Sheriyan's Coding School, Bhopal</h4>
+      </div>
     </div>
   )
 }
