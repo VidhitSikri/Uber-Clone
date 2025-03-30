c8e6bb21f08954a7e7381e33dfd1149a12c1a429
VidhitSikri <sikrividhit@gmail.com>
2025-03-30 16:40:28 +0530

    added captain logout

---
+++ frontend/src/pages/CaptainLogout.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+const CaptainLogout = () => {
+
+    const token = localStorage.getItem('token');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+            axios.get(`${import.meta.env.VITE_BASE_URL}/captains/logout`,{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then((response)=>{
+                if(response.status === 200){
+                    localStorage.removeItem('token');
+                    navigate("/captain-login");
+                }
+            })
+        }, [token, navigate])
+
+
+  return (
+    <div>
+      
+      i am a captains logout page
+    </div>
+  )
+}
+
+export default CaptainLogout
